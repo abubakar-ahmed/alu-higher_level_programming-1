9d5b0bb5b42906b92934f936fcc38ee64a32cb2c
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-07-11 00:58:02 -0700

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
-if _name_ == "_main_":
+if name == "main":
     import sys
     result = 0
     for arg in sys.argv:
