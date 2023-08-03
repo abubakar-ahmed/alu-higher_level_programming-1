3957accb26876d96b8805b45cc5aac0ff5963d7a
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-08-03 07:48:37 -0700

    task 2

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- command deletes the database htbn_0c_0 if it exist in MYSQL server. 
-DROP DATABASE IF  EXIST hbtn_0c_0
+-- command that deletes the database hbtn_0c_0 in your MySQL server.
+DROP DATABASE IF EXISTS hbtn_0c_0
