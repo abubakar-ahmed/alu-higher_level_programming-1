99a99d3fc2be97587f781242ddedf9c4367f68c5
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-08-13 15:26:35 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-""" prints the first State object from the
-database hbtn_0e_6_usa"""
+"""
+Script that prints the first `State` object from the database `hbtn_0e_6_usa`.
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    session = Session(bind=engine)
 
-    record = session.query(State).first()
+    instance = session.query(State).order_by(State.id).first()
 
-    if record:
-        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
+    if instance:
+        print("{}: {}".format(instance.id, instance.name))
     else:
         print("Nothing")
-
-    session.close()
