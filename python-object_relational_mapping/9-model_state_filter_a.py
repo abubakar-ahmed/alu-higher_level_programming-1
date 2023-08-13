a3d24ed2a721056287c9242fb0ab9d3c201c5ef5
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-08-13 15:51:16 -0700

    task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-"""A script that lists all records with letter a"""
-
+"""
+Script that lists all State objects containing the letter a from the database hbtn_0e_6_usa
+"""
 
 import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True
-                    )
-    Session = sessionmaker(bind=engine)
+    # Check if all 3 arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
+    # MySQL server configurations
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    host, port = "localhost", 3306
+
+    # Create the engine and bind it to the Base class
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@{host}:{port}/{database}",
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
 
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.contains('a')).all()
+    # Retrieve the states containing the letter 'a' sorted by ID
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
-    for state in states:
-        print(f'{state.id}: {state.name}')
+    # Display the results
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
 
+    # Close the session
     session.close()
