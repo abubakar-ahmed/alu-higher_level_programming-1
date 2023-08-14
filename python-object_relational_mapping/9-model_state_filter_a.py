268d01ef798df106842c7bba72e23bafa0928e14
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-08-14 03:01:14 -0700

    task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects containing the letter a from the database hbtn_0e_6_usa
-"""
+"""A script that lists all records with letter a"""
+
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-    # Check if all 3 arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True
+                    )
+    Session = sessionmaker(bind=engine)
 
-    # MySQL server configurations
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    host, port = "localhost", 3306
-
-    # Create the engine and bind it to the Base class
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@{host}:{port}/{database}",
-        pool_pre_ping=True
-    )
     Base.metadata.create_all(engine)
 
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Retrieve the states containing the letter 'a' sorted by ID
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.contains('a')).all()
 
-    # Display the results
-    for state in states_with_a:
-        print(f"{state.id}: {state.name}")
+    for state in states:
+        print(f'{state.id}: {state.name}')
 
-    # Close the session
     session.close()
